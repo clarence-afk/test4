135a21eb07a90faa2b3300825c26a0d9832a9a30
clarence-afk <carentan03@gmail.com>
2024-07-16 00:17:11 +0800

    Update VRButton.js

--- libs/VRButton.js
+++ libs/VRButton.js
@@ -66,60 +66,88 @@ class VRButton{
 
     }
 
-    showEnterVR(button) {
+	showEnterVR( button ) {
+
         let currentSession = null;
         const self = this;
+        
+        this.stylizeElement( button, true, 30, true );
+        
+        function onSessionStarted( session ) {
 
-        this.stylizeElement(button, true, 30, true);
+            session.addEventListener( 'end', onSessionEnded );
 
-        function onSessionStarted(session) {
-            session.addEventListener('end', onSessionEnded);
-            self.renderer.xr.setSession(session);
-            self.stylizeElement(button, false, 12, true);
+            self.renderer.xr.setSession( session );
+            self.stylizeElement( button, false, 12, true );
+            
             button.textContent = 'EXIT VR';
+
             currentSession = session;
-
+            
             if (self.onSessionStart !== undefined) self.onSessionStart();
+
         }
 
-        function onSessionEnded() {
-            currentSession.removeEventListener('end', onSessionEnded);
-            self.stylizeElement(button, true, 12, true);
+        function onSessionEnded( ) {
+
+            currentSession.removeEventListener( 'end', onSessionEnded );
+
+            self.stylizeElement( button, true, 12, true );
             button.textContent = 'ENTER VR';
-            currentSession = null;
 
+            currentSession = null;
+            
             if (self.onSessionEnd !== undefined) self.onSessionEnd();
+
         }
+
+        //
 
         button.style.display = '';
         button.style.right = '20px';
         button.style.width = '80px';
         button.style.cursor = 'pointer';
         button.innerHTML = '<i class="fas fa-vr-cardboard"></i>';
+        
 
         button.onmouseenter = function () {
-            button.style.fontSize = '12px';
-            button.textContent = (currentSession === null) ? 'ENTER VR' : 'EXIT VR';
+            
+            button.style.fontSize = '12px'; 
+            button.textContent = (currentSession===null) ? 'ENTER VR' : 'EXIT VR';
             button.style.opacity = '1.0';
-            self.stylizeHover(button, 'rgba(255, 255, 255, 0.8)'); // Change color on hover
+
         };
 
         button.onmouseleave = function () {
-            button.style.fontSize = '30px';
+            
+            button.style.fontSize = '30px'; 
             button.innerHTML = '<i class="fas fa-vr-cardboard"></i>';
             button.style.opacity = '0.5';
-            self.stylizeHover(button, 'rgba(20,150,80,1)'); // Revert color on leave
+
         };
 
         button.onclick = function () {
-            if (currentSession === null) {
-                navigator.xr.requestSession(self.sessionMode, self.sessionInit).then(onSessionStarted);
-            } else {
-                currentSession.end();
-            }
-        };
-    }
 
+            if ( currentSession === null ) {
+
+                // WebXR's requestReferenceSpace only works if the corresponding feature
+                // was requested at session creation time. For simplicity, just ask for
+                // the interesting ones as optional features, but be aware that the
+                // requestReferenceSpace call will fail if it turns out to be unavailable.
+                // ('local' is always available for immersive sessions and doesn't need to
+                // be requested separately.)
+
+                navigator.xr.requestSession( self.sessionMode, self.sessionInit ).then( onSessionStarted );
+
+            } else {
+
+                currentSession.end();
+
+            }
+
+        };
+
+    }
 
     disableButton(button) {
 
@@ -153,7 +181,9 @@ class VRButton{
 
     stylizeElement(element, active = true, fontSize = 13, ignorePadding = false) {
         element.style.position = 'absolute';
-        element.style.bottom = '20px';
+        element.style.top = '50%'; // Position the top edge of the button in the middle of the screen
+        element.style.left = '50%'; // Position the left edge of the button in the middle of the screen
+        element.style.transform = 'translate(-50%, -50%)'; // Move the button up and left by half its width and height to perfectly center it
         if (!ignorePadding) element.style.padding = '12px 6px';
         element.style.border = '1px solid #fff';
         element.style.borderRadius = '50%'; // Ensure the element remains circular
